084b2716e3a014983f64558718fe9d98e781d186
Bhavya Popat <bhavyabpopat7777@gmail.com>
2024-09-04 22:22:45 +0530

    Logo Fix + Adjusted Values for new Doctor Changes

--- src/index.js
+++ src/index.js
@@ -22,33 +22,17 @@ const coordinates = [
 ];
 
 function printLogo() {
+  console.log("\nWelcome to Dead By Daylight AFK Farming BOT (The Doctor Edition) by");
+  console.log("______ _                               ___           _   _____ _     _ _ _ ");
+  console.log("| ___ \\ |                             |_  |         | | /  __ \\ |   (_) | |");
+  console.log("| |_/ / |__   __ ___   ___   _  __ _    | |_   _ ___| |_| /  \\/ |__  _| | |");
+  console.log("| ___ \\ '_ \\ / _` \\ \\ / / | | |/ _` |   | | | | / __| __| |   | '_ \\| | | |");
+  console.log("| |_/ / | | | (_| |\\ V /| |_| | (_| /\\__/ / |_| \\__ \\ |_| \\__/\\ | | | | | |");
   console.log(
-    "\nWelcome to Dead By Daylight AFK Farming BOT (The Doctor Edition) by"
+    "\\____/|_| |_|\\__,_| \\_/  \\__, |\\__,_\\____/ \\__,_|___/\\__|\\____/_| |_|_|_|_|"
   );
-  console.log(
-    "______ _                               ___           _   _____ _     _ _ _ "
-  );
-  console.log(
-    "| ___ \\ |                             |_  |         | | /  __ \\ |   (_) | |"
-  );
-  console.log(
-    "| |_/ / |__   __ ___   ___   _  __ _    | |_   _ ___| |_| /  \\/ |__  _| | |"
-  );
-  console.log(
-    "| ___ \\ '_ \\ / _` \\ \\ / / | | |/ _` |   | | | | / __| __| |   | '_ \\| | | |"
-  );
-  console.log(
-    "| |_/ / | | | (_| |\\ V /| |_| | (_| /\\__/ / |_| \\__ \\ |_| \\__/\\ | | | | | |"
-  );
-  console.log(
-    "\\____/|_| |_|__,_| \\_/  \\__, |\\__,_\\____/ \\__,_|___/\\__|\\____/_| |_|_|_|_|"
-  );
-  console.log(
-    "                          __/ |                                            "
-  );
-  console.log(
-    "                         |___/                                             "
-  );
+  console.log("                          __/ |                                            ");
+  console.log("                         |___/                                             ");
 }
 
 function intro() {
@@ -62,22 +46,18 @@ function intro() {
     input: process.stdin,
     output: process.stdout,
   });
-  rl.question(
-    "Enter the number corresponding to your resolution choice: ",
-    (answer) => {
-      try {
-        const index = parseInt(answer) - 1;
-        if (index < 0 || index >= coordinates.length)
-          throw new Error("Invalid choice");
-        clickCoordinates = coordinates[index];
-      } catch (e) {
-        console.log("\nInvalid choice!\nDefaulting to 1920 x 1080 resolution.");
-        clickCoordinates = coordinates[0];
-      }
-      rl.close();
-      startBot();
+  rl.question("Enter the number corresponding to your resolution choice: ", (answer) => {
+    try {
+      const index = parseInt(answer) - 1;
+      if (index < 0 || index >= coordinates.length) throw new Error("Invalid choice");
+      clickCoordinates = coordinates[index];
+    } catch (e) {
+      console.log("\nInvalid choice!\nDefaulting to 1920 x 1080 resolution.");
+      clickCoordinates = coordinates[0];
     }
-  );
+    rl.close();
+    startBot();
+  });
 }
 
 function rightClick(delay) {
@@ -124,10 +104,8 @@ function toggleRunning() {
 
 function moveCameraRandomly(currentX, currentY) {
   const maxMovement = 1400;
-  const newX =
-    currentX + Math.floor(Math.random() * (2 * maxMovement + 1)) - maxMovement;
-  const newY =
-    currentY + Math.floor(Math.random() * (2 * maxMovement + 1)) - maxMovement;
+  const newX = currentX + Math.floor(Math.random() * (2 * maxMovement + 1)) - maxMovement;
+  const newY = currentY + Math.floor(Math.random() * (2 * maxMovement + 1)) - maxMovement;
   robot.moveMouseSmooth(newX, newY, 2);
 }
 
@@ -140,7 +118,7 @@ async function botActions() {
     click(clickCoordinates.popup.x, clickCoordinates.popup.y);
     await sleep(3000);
     pressReleaseCtrl(3000);
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 5; i++) {
       if (!running) return;
       rightClick(3000);
       if (i % 5 === 0) {
@@ -162,6 +140,12 @@ async function botActions() {
     moveCameraRandomly(x, y);
     await sleep(2000);
     randomDirection();
+    click(clickCoordinates.ready.x, clickCoordinates.ready.y);
+    await sleep(1000);
+    click(clickCoordinates.continue.x, clickCoordinates.continue.y);
+    await sleep(1000);
+    click(clickCoordinates.popup.x, clickCoordinates.popup.y);
+    await sleep(3000);
   }
 }
 
