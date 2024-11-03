99f28a28ac39ad2fb88d88e364d0dec60f3052b8
Bhavya Popat <bhavyabpopat7777@gmail.com>
2024-11-03 11:14:45 +0530

    Added Dark Lord killer actions

--- src/index.js
+++ src/index.js
@@ -73,7 +73,9 @@ const killerActions = [
             await sleep(300);
           }
         };
-        await Promise.all([spamRightClick(), moveAndChangeDirection()]);
+        if (running) {
+          await Promise.all([spamRightClick(), moveAndChangeDirection()]);
+        }
       }
     },
   },
@@ -95,36 +97,32 @@ const killerActions = [
       }
     },
   },
+  {
+    name: "The Dark Lord",
+    actions: async function () {
+      for (let i = 0; i < 10; i++) {
+        if (!running) return;
+        robot.keyToggle("control", "down");
+        robot.mouseClick("left");
+        robot.keyToggle("control", "up");
+        await sleep(3000);
+      }
+    },
+  },
 ];
 
 function printLogo() {
-  console.log(
-    "\nWelcome to Dead By Daylight AFK Farming BOT (The Doctor Edition) by"
-  );
-  console.log(
-    "______ _                               ___           _   _____ _     _ _ _ "
-  );
-  console.log(
-    "| ___ \\ |                             |_  |         | | /  __ \\ |   (_) | |"
-  );
-  console.log(
-    "| |_/ / |__   __ ___   ___   _  __ _    | |_   _ ___| |_| /  \\/ |__  _| | |"
-  );
-  console.log(
-    "| ___ \\ '_ \\ / _` \\ \\ / / | | |/ _` |   | | | | / __| __| |   | '_ \\| | | |"
-  );
-  console.log(
-    "| |_/ / | | | (_| |\\ V /| |_| | (_| /\\__/ / |_| \\__ \\ |_| \\__/\\ | | | | | |"
-  );
+  console.log("\nWelcome to Dead By Daylight AFK Farming BOT by");
+  console.log("______ _                               ___           _   _____ _     _ _ _ ");
+  console.log("| ___ \\ |                             |_  |         | | /  __ \\ |   (_) | |");
+  console.log("| |_/ / |__   __ ___   ___   _  __ _    | |_   _ ___| |_| /  \\/ |__  _| | |");
+  console.log("| ___ \\ '_ \\ / _` \\ \\ / / | | |/ _` |   | | | | / __| __| |   | '_ \\| | | |");
+  console.log("| |_/ / | | | (_| |\\ V /| |_| | (_| /\\__/ / |_| \\__ \\ |_| \\__/\\ | | | | | |");
   console.log(
     "\\____/|_| |_|\\__,_| \\_/  \\__, |\\__,_\\____/ \\__,_|___/\\__|\\____/_| |_|_|_|_|"
   );
-  console.log(
-    "                          __/ |                                            "
-  );
-  console.log(
-    "                         |___/                                             "
-  );
+  console.log("                          __/ |                                            ");
+  console.log("                         |___/                                             ");
 }
 
 function selectKiller(rl) {
@@ -134,23 +132,19 @@ function selectKiller(rl) {
       console.log(`${i + 1}. ${killer.name}`);
     });
 
-    rl.question(
-      "Enter the number corresponding to your killer choice: ",
-      (answer) => {
-        try {
-          const index = parseInt(answer) - 1;
-          if (index < 0 || index >= killerActions.length)
-            throw new Error("Invalid choice");
-          selectedKillerActions = killerActions[index].actions;
-          console.log(`\nKiller set to: ${killerActions[index].name}`);
-          resolve();
-        } catch (e) {
-          console.log("\nInvalid choice! Defaulting to The Doctor.");
-          selectedKillerActions = killerActions[0].actions;
-          resolve();
-        }
+    rl.question("Enter the number corresponding to your killer choice: ", (answer) => {
+      try {
+        const index = parseInt(answer) - 1;
+        if (index < 0 || index >= killerActions.length) throw new Error("Invalid choice");
+        selectedKillerActions = killerActions[index].actions;
+        console.log(`\nKiller set to: ${killerActions[index].name}`);
+        resolve();
+      } catch (e) {
+        console.log("\nInvalid choice! Defaulting to The Doctor.");
+        selectedKillerActions = killerActions[0].actions;
+        resolve();
       }
-    );
+    });
   });
 }
 
@@ -166,25 +160,21 @@ function intro() {
     output: process.stdout,
   });
 
-  rl.question(
-    "Enter the number corresponding to your resolution choice: ",
-    (answer) => {
-      try {
-        const index = parseInt(answer) - 1;
-        if (index < 0 || index >= coordinates.length)
-          throw new Error("Invalid choice");
-        clickCoordinates = coordinates[index];
-      } catch (e) {
-        console.log("\nInvalid choice! Defaulting to 1920 x 1080 resolution.");
-        clickCoordinates = coordinates[0];
-      }
-
-      selectKiller(rl).then(() => {
-        rl.close();
-        startBot();
-      });
+  rl.question("Enter the number corresponding to your resolution choice: ", (answer) => {
+    try {
+      const index = parseInt(answer) - 1;
+      if (index < 0 || index >= coordinates.length) throw new Error("Invalid choice");
+      clickCoordinates = coordinates[index];
+    } catch (e) {
+      console.log("\nInvalid choice! Defaulting to 1920 x 1080 resolution.");
+      clickCoordinates = coordinates[0];
     }
-  );
+
+    selectKiller(rl).then(() => {
+      rl.close();
+      startBot();
+    });
+  });
 }
 
 function rightClick(delay) {
@@ -231,10 +221,8 @@ function toggleRunning() {
 
 function moveCameraRandomly(currentX, currentY) {
   const maxMovement = 1000;
-  const newX =
-    currentX + Math.floor(Math.random() * (2 * maxMovement + 1)) - maxMovement;
-  const newY =
-    currentY + Math.floor(Math.random() * (2 * maxMovement + 1)) - maxMovement;
+  const newX = currentX + Math.floor(Math.random() * (2 * maxMovement + 1)) - maxMovement;
+  const newY = currentY + Math.floor(Math.random() * (2 * maxMovement + 1)) - maxMovement;
   robot.moveMouseSmooth(newX, newY, 2);
 }
 
